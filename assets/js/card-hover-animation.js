4cde774279f520ec5e292c500f3c513d3e752d43
Matheus Eyhad Dias sales Costa <dev.eyhad@gmail.com>
2023-02-22 19:03:21 -0300

    update

--- assets/js/card-hover-animation.js
+++ assets/js/card-hover-animation.js
@@ -1,9 +1,11 @@
 function handleMouseEnter(){
     this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}-hovered`;
 }
 
 function headleMouseLeave(){
     this.classList.remove('s-card--hovered');
+    document.body.id = '';
 }
 
 function addEventListenerToCards(){
